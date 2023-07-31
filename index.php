1280aaa286f1709def0d5ebcd23298281a89d4d4
iO Academy <thejoshsilasbryan@gmail.com>
2023-07-31 16:29:55 +0100

    Task 3 Have a HTML and CSS layout ready to recieve the database

--- index.php
+++ index.php
@@ -1,10 +1,30 @@
 <?php 
-$db = new PDO('mysql:host=db;dbname=boardgames', 'root', 'password');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$query = $db ->prepare("SELECT `name`, `description`, `playercount`, `difficulty` FROM `boardgames`");
-$query->execute();
-$data = $query->fetchAll();
-echo '<pre>';
-var_dump($data);
-echo '</pre>';
+require('functions.php');
+
+?> 
+
+<html lang="en-GB"> 
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<title>Theboardgamecollection</title>
+	</head>
+	<body>
+        <header>
+            <h1> Boardgame Collection </h1>
+        </header>
+		<div>
+        	<section class = "game">			
+				<h2> NAME FROM THE DATA BASE GOES HERE </h2>
+				<p>STAT 2 PLAYER COUNT - 2 - 4 </p>
+				<p> STAT 3 DIFFICULTY - 3</p>
+				<p> STAT 1 DESRICPTION blah blah blah blah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blah</p>
+			</section>	
+
+			<section class = "game">			
+				<h2> NAME FROM THE DATA BASE GOES HERE </h2>
+				<p>STAT 2 PLAYER COUNT</p>
+				<p>STAT 3 DIFFICULTY</p>
+				<p> STAT 1 DESRICPTION blah blah blah blah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blah</p>
+			</section>
+			</div> 
+</body>

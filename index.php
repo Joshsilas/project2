695d5caf0db2888c9501dcc638607bce88136190
iO Academy <thejoshsilasbryan@gmail.com>
2023-08-01 15:14:17 +0100

    story1task4 ready for peer review 4

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-require_once('CollectionCardMaker.php');
+require_once('src/CollectionCardMaker.php');
 $db = new PDO('mysql:host=db;dbname=boardgames', 'root', 'password');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
@@ -19,9 +19,9 @@ $boardgames = $query->fetchAll();
         </header>
 		<div class = "container">
 			<?php
-			foreach ($boardgames as $game){
-				$collectioncardmaker = new CollectionCardMaker($game['name'], $game['playercount'], $game['difficulty'], $game['description']);
-				echo $collectioncardmaker->createGameCard();
+			foreach ($boardgames as $boardgame){
+				$collectionCardMaker = new CollectionCardMaker($boardgame['name'], $boardgame['playercount'], $boardgame['difficulty'], $boardgame['description']);
+				echo $collectionCardMaker->createGameCard();
 			}
 			?>
 		</div> 
---
+++ src/CollectionCardMaker.php
@@ -0,0 +1,26 @@
+<?php
+
+class CollectionCardMaker {
+    public string $name;
+    public string $playercount;
+    public string $difficulty;
+    public string $description;
+
+    public function __construct($name, $playercount, $difficulty, $description)
+    {
+         $this->name = $name;
+         $this->playercount = $playercount;
+         $this->difficulty = $difficulty;
+         $this->description = $description;
+    }
+    public function createGameCard()
+    {
+     return 
+        "<section class = 'board-game-section'>		
+		    <h2>$this->name</h2>
+		    <p>The Player Count is $this->playercount players</p>
+		    <p>The difficulty to learn is $this->difficulty/5</p>
+		    <p>$this->description</p>
+	    </section>";
+    }
+}

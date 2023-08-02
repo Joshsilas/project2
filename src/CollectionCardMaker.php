980584c89f11645d815b9d3ea211afcd9c0a30ff
iO Academy <thejoshsilasbryan@gmail.com>
2023-08-02 11:38:52 +0100

    Story1task5 Ready for peer review 1

--- src/CollectionCardMaker.php
+++ src/CollectionCardMaker.php
@@ -6,7 +6,7 @@ class CollectionCardMaker {
     private string $difficulty;
     private string $description;
 
-    public function __construct($name, $playercount, $difficulty, $description)
+    public function __construct(string $name,string $playercount,int $difficulty,string $description)
     {
          $this->name = $name;
          $this->playercount = $playercount;
@@ -18,9 +18,9 @@ class CollectionCardMaker {
      return 
     "<section class = 'board-game-section'>		
         <h2>$this->name</h2>
-        <p>The Player Count is $this->playercount players</p>
+        <p>The player count is $this->playercount players</p>
         <p>The difficulty to learn is $this->difficulty/5</p>
         <p>$this->description</p>
     </section>";
-}
+    }
 }
--- tests/collectioncardmakertest.php
+++ tests/collectioncardmakertest.php
@@ -6,10 +6,11 @@ use PHPUnit\Framework\TestCase;
 
 class CollectionCardMakerTest extends TestCase {
 
-public function testcreateGameCard() 
+
+public function testcreateGameCard()
 {
    
-    $name = 'wow';
+    $name = 1;
     $playercount = '2 - 4';
     $difficulty = '2';
     $description = 'blah';
@@ -17,7 +18,7 @@ public function testcreateGameCard()
     $creategamecard = new CollectionCardMaker($name, $playercount, $difficulty, $description);
     $outcome = 
     "<section class = 'board-game-section'>		
-        <h2>wow</h2>
+        <h2>1</h2>
         <p>The Player Count is 2 - 4 players</p>
         <p>The difficulty to learn is 2/5</p>
         <p>blah</p>

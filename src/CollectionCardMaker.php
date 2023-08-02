4c4abffbc9edfecd7a3c01152d8f139ea54242f2
iO Academy <thejoshsilasbryan@gmail.com>
2023-08-02 10:55:10 +0100

    Stor1task5 almost ready for review

--- src/CollectionCardMaker.php
+++ src/CollectionCardMaker.php
@@ -16,11 +16,11 @@ class CollectionCardMaker {
     public function createGameCard()
     {
      return 
-        "<section class = 'board-game-section'>		
-		    <h2>$this->name</h2>
-		    <p>The Player Count is $this->playercount players</p>
-		    <p>The difficulty to learn is $this->difficulty/5</p>
-		    <p>$this->description</p>
-	    </section>";
-    }
+    "<section class = 'board-game-section'>		
+        <h2>$this->name</h2>
+        <p>The Player Count is $this->playercount players</p>
+        <p>The difficulty to learn is $this->difficulty/5</p>
+        <p>$this->description</p>
+    </section>";
+}
 }
---
+++ tests/collectioncardmakertest.php
@@ -0,0 +1,29 @@
+<?php
+
+require('../src/collectioncardmaker.php');
+
+use PHPUnit\Framework\TestCase;
+
+class CollectionCardMakerTest extends TestCase {
+
+public function testcreateGameCard() 
+{
+   
+    $name = 'wow';
+    $playercount = '2 - 4';
+    $difficulty = '2';
+    $description = 'blah';
+
+    $creategamecard = new CollectionCardMaker($name, $playercount, $difficulty, $description);
+    $outcome = 
+    "<section class = 'board-game-section'>		
+        <h2>wow</h2>
+        <p>The Player Count is 2 - 4 players</p>
+        <p>The difficulty to learn is 2/5</p>
+        <p>blah</p>
+    </section>";
+
+$result = $creategamecard->createGameCard();
+    $this->assertEquals($outcome, $result);
+}
+}

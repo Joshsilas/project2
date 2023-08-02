7bd22224ed24344b53b3ec0ccdb28092f57c41a2
iO Academy <thejoshsilasbryan@gmail.com>
2023-08-02 12:14:09 +0100

    story1task5 ready for review 2

--- tests/collectioncardmakertest.php
+++ tests/collectioncardmakertest.php
@@ -10,7 +10,7 @@ class CollectionCardMakerTest extends TestCase {
 public function testcreateGameCard()
 {
    
-    $name = 1;
+    $name = 'wow';
     $playercount = '2 - 4';
     $difficulty = '2';
     $description = 'blah';
@@ -18,8 +18,8 @@ public function testcreateGameCard()
     $creategamecard = new CollectionCardMaker($name, $playercount, $difficulty, $description);
     $outcome = 
     "<section class = 'board-game-section'>		
-        <h2>1</h2>
-        <p>The Player Count is 2 - 4 players</p>
+        <h2>wow</h2>
+        <p>The player count is 2 - 4 players</p>
         <p>The difficulty to learn is 2/5</p>
         <p>blah</p>
     </section>";

045319233bd5f0e0043c8ca198d9fac4b98ec357
iO Academy <thejoshsilasbryan@gmail.com>
2023-08-03 10:50:17 +0100

    Story 1 Ready for trainer review 2

--- index.php
+++ index.php
@@ -1,27 +1,33 @@
 <?php 
-require_once('src/CollectionCardMaker.php');
+require_once('src/Game.php');
+
 $db = new PDO('mysql:host=db;dbname=boardgames', 'root', 'password');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$query = $db ->prepare("SELECT `name`, `description`, `playercount`, `difficulty` FROM `boardgames`");
+$query = $db->prepare("SELECT `name`, `description`, `playercount`, `difficulty` FROM `boardgames`");
 $query->execute();
-$boardgames = $query->fetchAll();
+$boardGames = $query->fetchAll();
+
+$boardGamesAsObjects = [];
+foreach ($boardGames as $boardGame) {
+$game = new Game($boardGame['name'], $boardGame['playercount'], $boardGame['difficulty'], $boardGame['description']);
+array_push($boardGamesAsObjects, $game);
+}
 ?> 
 
 <html lang="en-GB"> 
 	<head>
-		<link rel="stylesheet" type="text/css" href="src/style.css">
-		<title>The boardgame collection</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>The boardgame collection</title>
 	</head>
 	<body>
         <header>
             <h1>Boardgame Collection</h1>
         </header>
-		<div class = "card-container">
+		<div class="card-container">
 			<?php
-			foreach ($boardgames as $boardgame){
-				$collectionCardMaker = new CollectionCardMaker($boardgame['name'], $boardgame['playercount'], $boardgame['difficulty'], $boardgame['description']);
-				echo $collectionCardMaker->createGameCard();
+			foreach ($boardGamesAsObjects as $boardGameAsObject) {
+				echo $boardGameAsObject->createGameCard();
 			}
 			?>
 		</div> 
---
+++ tests/GameTest.php
@@ -0,0 +1,28 @@
+<?php
+
+require('../src/Game.php');
+
+use PHPUnit\Framework\TestCase;
+
+class GameTest extends TestCase {
+
+    public function test_success_create_game_card()
+    {   
+    $name = 'wow';
+    $playerCount = '2 - 4';
+    $difficulty = '2';
+    $description = 'blah';
+
+    $game = new Game($name, $playerCount, $difficulty, $description);
+    $expectedResult = 
+        '<section class="board-game-section"> 		
+            <h2>' . 'wow' . '</h2> 
+            <p>The player count is ' . '2 - 4' . ' players</p> 
+            <p>The difficulty to learn is ' . '2' . '/5</p>
+            <p>' . 'blah' . '</p>
+        </section>';
+
+    $actualResult = $game->createGameCard();
+    $this->assertEquals($expectedResult, $actualResult);
+    }
+}
